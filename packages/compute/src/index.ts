5f99cc32c094e7cb63eff4c77f5a3bce21a9ea97
Florian <florian@technotro.com>
2026-04-30 20:01:10 +0700

    integrate 0g router for compute (tbd)

---
+++ packages/compute/src/index.ts
@@ -0,0 +1,9 @@
+export {
+  ZERO_G_COMPUTE_ROUTER_MAINNET_BASE_URL,
+  ZERO_G_COMPUTE_ROUTER_TESTNET_BASE_URL,
+  createZeroGComputeRouterChatCompletionBody,
+  zeroGComputeRouterLlmClient,
+  type ZeroGComputeRouterFetch,
+  type ZeroGComputeRouterLlmClientOptions,
+  type ZeroGComputeRouterResponseFormatStrategy,
+} from './router-client.js';
